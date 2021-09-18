40049c4e6d55dd18e51af96ef81e13ecfa90d1a8
PH <iui4rar@gmail.com>
2021-09-18 20:49:49 +0200

    version 0.1.1 RC

--- src/components/Stundenplan.vue
+++ src/components/Stundenplan.vue
@@ -65,49 +65,17 @@
       </div>
     </div>
 
-    <table class="table table-striped table-hover table-bordered">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Montag</th>
-          <th scope="col">Dienstag</th>
-          <th scope="col">Mittwoch</th>
-          <th scope="col">Donnerstag</th>
-          <th scope="col">Freitag</th>
-        </tr>
-      </thead>
-      <tbody>          
-        <stundenplan-row 
-          v-for="row in studTable" 
-          v-bind:key="row.id"
-          v-bind:row="row"
-          :hover="hoover"
-          @box-selected="onHoover">
-        </stundenplan-row>
-      </tbody>
-    </table> 
-
-    <div class="container">
-      <div class="card">
-        <div class="card-header">
-          Timeless
-        </div>
-      
-        <div class="card-body row row-cols-auto">
-          <div class="col" v-for="(vorl,i) in timelessTable" scope="col" :id="'col_TL_'+i" :key="vorl.id">
-            <vorlesungsBox :data="vorl" @box-hoover="onHoover" :hover="hoover"></vorlesungsBox>
-          </div>
-        </div>
-      </div>
-    </div>
+    <timetable 
+      :studTable="studTable"
+      :hoover="hoover"
+      :selected="selected"
+      :timelessTable="timelessTable" />
           
 </template>
 
 <script>
-
-import stundenplanRow from "./StundenplanRow.vue";
-import vorlesungsBox from "./VorlesungsBox.vue";
 import usersData from "../assets/data.json";
+import timetable from './Timetable.vue';
 
 function createDataArray() {
   return [
@@ -133,8 +101,7 @@ export default {
         }
       },
     components: {
-        stundenplanRow,
-        vorlesungsBox
+        timetable
     },
     methods: {
         addToTable(element) {
